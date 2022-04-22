19658449d43e2513333683fe259565f21a8418c3
Anara Zhunusova <anarikrulit@gmail.com>
2022-04-22 20:21:13 +0200

    redacted code, added try-with-resources

--- src/Main.java
+++ src/Main.java
@@ -6,43 +6,44 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 // SERVER SIDE
+
 public class Main {
     public static void main(String[] args) {
         final int port = 8080;
 
         // connection
-        try {
-            // connection established, server waiting for a message from client
-            System.out.println("Waiting for client message...");
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket socket = serverSocket.accept();
+             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream())) {
 
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket socket = serverSocket.accept();
-            // send a message to client
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            oos.writeObject("Hi! Please enter your name");
+                // connection established, server waiting for a message from client
+                System.out.println("Waiting for client message...");
 
-            // Read a message sent by client application
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-            String message = (String) ois.readObject();
-            System.out.println("Message Received: " + message);
+                // send a message to client
+                oos.writeObject("Hi! Please enter your name");
 
-            // Send a response information to the client application
-            oos.writeObject("Hi, " + message + "! Are you child? (yes/no)");
+                // Read a message sent by client application
+                String message = (String) ois.readObject();
+                System.out.println("Message Received: " + message);
 
-            // read a reply from client and send a message
-            String message2 = (String) ois.readObject();
-            System.out.println("Message Received: " + message2);
+                // Send a response information to the client application
+                oos.writeObject("Hi, " + message + "! Are you child? (yes/no)");
 
-            if (message2.equals("yes")) {
-                oos.writeObject("Welcome to the kids area, " + message + "! Let's play!");
-            } else {
-                oos.writeObject("Welcome to the adult zone, " + message + "! Have a good rest, or a good working day!");
-            }
+                do {
+                    // read a reply from client and send a message
+                    String message2 = (String) ois.readObject();
+                    System.out.println("Message Received: " + message2);
 
-            ois.close();
-            oos.close();
-            socket.close();
-
+                    if (message2.equals("yes")) {
+                        oos.writeObject("Welcome to the kids area, " + message + "! Let's play!");
+                        break;
+                    } else if (message2.equals("no")) {
+                        oos.writeObject("Welcome to the adult zone, " + message + "! Have a good rest, or a good working day!");
+                        break;
+                    }
+                        oos.writeObject("Please enter (yes/no)");
+                } while (true);
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
